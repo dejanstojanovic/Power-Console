e2d0c9461e4df20d5cfeb986d32a2e3a95e2a0d4
Dejan Stojanovic <d.stojanovic@hotmail.com>
2015-05-26 17:12:15 +0400

    Updated assembly info

--- ConsoleExtended/Properties/AssemblyInfo.cs
+++ ConsoleExtended/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ConsoleExtended")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extended System.Console wrapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dejanstojanovic.net")]
 [assembly: AssemblyProduct("ConsoleExtended")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
